cefb49fd14da4a1a40f9022f45518157f1f52576
Yadzyajaida <118242421+Yadzyajaida@users.noreply.github.com>
2026-01-31 12:38:23 -0500

    Update page.tsx

--- src/app/remote-id-generator/page.tsx
+++ src/app/remote-id-generator/page.tsx
@@ -9,10 +9,10 @@ export default function RemoteIdGeneratorPage() {
       <header className="py-8 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto justify-items-center">
           <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl lg:text-6xl font-headline">
-            Generador de Remote ID
+            Generador de remote ID
           </h1>
           <p className="mt-4 max-w-3xl text-lg text-muted-foreground">
-            Crea Remote IDs estandarizados para locales de forma masiva.
+            Crea remote ID's estandarizados para integrar locales de forma masiva.
           </p>
         </div>
       </header>
