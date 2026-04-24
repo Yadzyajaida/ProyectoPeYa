55534b337bbb86ad0871d9f048f47ac285bee54f
yadirayajaida <yadirayajaida@gmail.com>
2026-04-24 15:50:27 +0000

    intento

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,16 +10,12 @@ const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ user }) {
-      // ESTA ES LA MAGIA: Solo permite correos de pedidosya.com
       if (user.email?.endsWith("@pedidosya.com")) {
         return true;
       }
-      return false; // Si no es PeYa, rechaza el login
+      return false; 
     },
   },
-  pages: {
-    signIn: '/auth/signin', // Aquí puedes crear tu propia página con el botón
-  }
-});
+}); 
 
 export { handler as GET, handler as POST };
