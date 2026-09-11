eb16b224ec0998e31a7f63c9b6e5c56cd924fbbb
yadirayajaida <yadirayajaida@gmail.com>
2026-09-11 20:31:38 +0000

    dl

--- src/components/xlsx-processor-client-page.tsx
+++ src/components/xlsx-processor-client-page.tsx
@@ -28,7 +28,7 @@ export default function XlsxProcessorClientPage() {
                 <h2 className="text-xl font-semibold text-foreground">Puntos clave del procesador</h2>
                 <ul className="list-disc pl-5 text-muted-foreground text-sm space-y-2">
                     <li>Este procesador es exclusivo para el proceso de <strong>codificar menú</strong>.</li>
-                    <li>Los archivos a subir deben estar en formato <strong>XLSX</strong> y tener las <strong>mismas columnas</strong> que los archivos descargados de <strong>Backoffice</strong>.</li>
+                    <li>Los archivos a subir deben estar en formato <strong>XLSX</strong> y tener las <strong>mismas columnas</strong> que los archivos descargados de <strong>Backoffice (si cuenta con alguna columna adicional no hay problema)</strong>.</li>
                     <li>No necesitas corregir SKUs vacíos, duplicados, ni eliminar columnas. El sistema se encarga de todo automáticamente.</li>
                     <li>El sistema revisará que las columnas cumplan con el formato adeacuado según las indicaciones de <strong>Backoffice</strong>.</li>
                     <li>Si el archivo de opcionales cuenta con SKUs de opcionales repetidos, <strong>dentro de un mismo grupo opcional</strong>, el sistema le antepone una letra para que puedan ser subidos <strong>(debido a un error del sistema Backoffice, donde solo sube el último opcional con SKU repetido)</strong> .</li>
